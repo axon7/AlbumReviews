4b3ed9bb81c5300976f399f910eeb4b2c9a00e83
Kuba <kubaz9991@gmail.com>
2019-07-25 22:14:54 +0200

    mongodb config

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+//connects with database
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 app.listen(PORT, () => {
