2d1edda48e4cc3ddf3013f0e554884aec26d670b
Kuba <kubaz9991@gmail.com>
2019-10-10 23:35:24 +0200

    Initial client setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <p>Album reviews</p>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
