e66c31eeff8eac80a8dc3e9619e918d9de191c83
Kuba <kubaz9991@gmail.com>
2019-10-15 22:41:00 +0200

    Add home and search routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import NewReleases from "./containers/NewReleases";
+import BrowseAlbums from "./containers/BrowseAlbums";
 import Login from "./components/authorization/Login";
 import Register from "./components/authorization/Register";
 import { Provider } from "react-redux";
 import store from "./store";
+import SearchAlbums from "./containers/SearchAlbums";
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={NewReleases} />
+          <Route exact path='/' component={BrowseAlbums} />
+          <Route exact path='/search' component={SearchAlbums} />
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
         </Switch>
