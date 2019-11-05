d8b17ff4b84961890071dcce580f4622e6ca585c
Kuba <kubaz9991@gmail.com>
2019-11-05 10:00:08 +0100

    Hide nav links for guests

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const Navbar = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { logout } from '../../actions/authentication';
+
+const Navbar = ({ authentication: { isAuthenticated, loading }, logout }) => {
+  const loggedUserLinks = (
+    <ul>
+      <li>
+        <Link to="/">Browse albums</Link>
+      </li>
+      <li>
+        <Link to="/search">Search albums</Link>
+      </li>
+      <li>
+        <Link to="/" onClick={() => logout()}>
+          Logout
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <header>
       <nav>
-        <ul>
-          <li>
-            <Link to='/'>Browse albums</Link>
-          </li>
-          <li>
-            <Link to='/search'>Search albums</Link>
-          </li>
-          <li>
-            <Link to='/register'>Register</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-        </ul>
+        <h1>
+          <Link to="/">ALBUM REVIEWS</Link>
+        </h1>
       </nav>
+      {!loading && <>{isAuthenticated ? loggedUserLinks : guestLinks}</>}
     </header>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  authentication: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  authentication: state.authentication
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
