03179a3ec5cdc2b02c8da68bb4e793818265025d
Kuba <kubaz9991@gmail.com>
2019-11-06 21:20:52 +0100

    Add ability to search for album by title

--- client/src/containers/BrowseAlbums.js
+++ client/src/containers/BrowseAlbums.js
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
-export default class BrowseAlbums extends Component {
-  render() {
-    return (
-      <div>
-        <div>all albums from database</div>
-      </div>
-    );
-  }
-}
+import React from 'react';
+
+const BrowseAlbums = () => {
+  return (
+    <div>
+      <div>if there are no albums in database, show => no albums</div>
+      <div>else</div>
+      <div>fetch all albums from database and render it here</div>
+    </div>
+  );
+};
+
+export default BrowseAlbums;
--- client/src/containers/SearchAlbums.js
+++ client/src/containers/SearchAlbums.js
@@ -1,26 +1,40 @@
-import React from "react";
-import axios from "axios";
-class SearchAlbums extends React.Component {
-  componentDidMount() {
-    axios
-      .get(
-        'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q=album:"sweet"'
-      )
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
-  render() {
-    return (
-      <form>
-        <input name='searchTerm' placeholder='Search album' type='text' />
-        <input type='submit' />
-      </form>
-    );
-  }
-}
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { fetchAlbums } from '../actions/search';
+import AlbumsList from '../components/AlbumsList';
 
-export default SearchAlbums;
+const SearchAlbums = ({ fetchAlbums, data }) => {
+  const [search, setSearchTerm] = useState();
+
+  const handleSearch = e => {
+    e.preventDefault();
+    console.log(search);
+    //run fetching
+    fetchAlbums(search);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSearch}>
+        <input
+          name="searchTerm"
+          placeholder="Search album"
+          type="text"
+          value={search}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+      <AlbumsList data={data} />
+    </>
+  );
+};
+
+const mapStateToProps = state => ({
+  data: state.search.data
+});
+export default connect(
+  mapStateToProps,
+  { fetchAlbums }
+)(SearchAlbums);
